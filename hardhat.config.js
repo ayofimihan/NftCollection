8c4361260cc4fe7a346e3904a2217885562e1980
ayofimihan <cogunsina@gmail.com>
2022-10-09 03:51:45 +0100

    deploy script and network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks:{
+    goerli:{
+      url: process.env.ALCHEMY_API_KEY_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,22 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+//ethers.js comes with hardhat
+//import ethers 
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+const {ethers} = require("hardhat")
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+async function main(){
+//tell the script we want to deploy NFTea.sol contract
+const contract = await ethers.getContractFactory('NFTea');
+//deploy it
+const deployedContract = await contract.deploy();
+//wait for the contract to finish deploying
+await deployedContract.deployed();
+//print the address of the deployed contract
+console.log(deployedContract.address)
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error)=>{
   console.log(error);
-  process.exit(1);
+  process.exit(1); 
 })
